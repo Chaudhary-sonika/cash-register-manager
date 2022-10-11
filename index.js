cde09a1369718842ecefedaf18daab922a581fc4
Chaudhary-sonika <111914130+Chaudhary-sonika@users.noreply.github.com>
2022-10-11 16:38:53 +0530

    feat: added next button

--- index.js
+++ index.js
@@ -1,39 +1,51 @@
-const billAmount = document.querySelector("#bill-amount");
-const cashGiven = document.querySelector("#cash-given");
-const checkButton = document.querySelector("#check-button");
-const message = document.querySelector("#error-message");
-const noOfNotes = document.querySelectorAll(".no-of-notes");
+const billingAmt = document.querySelector("#billing");
+const cashInput = document.querySelector("#cash-given");
+const submitButton = document.querySelector("#submit-button");
+const errorMsg = document.querySelector("#error-msg");
+const noteNumbers = document.querySelectorAll(".no-of-notes");
+const next = document.querySelector("#next-button");
+const cashContainer = document.querySelector(".container-a");
+const tableContainer = document.querySelector(".container-b");
 
+const notesAvailable = [ 2000, 500, 100, 20, 10, 5, 1 ];
 
-const availableNotes = [ 2000, 500, 100, 20, 10, 5, 1 ];
-
-checkButton.addEventListener("click", function validateBillAndCashAmount() {
-    hideMessage();
-    if (billAmount.value > 0) {
-        if (Number(cashGiven.value) >= Number(billAmount.value)) {
-            const amountToBeReturned = cashGiven.value - billAmount.value;
-            calculateChange(amountToBeReturned);
-        } else {
-            showMessage("Do you wanna wash plates?");
-        }
+function checkBillingInput() {
+    if (billingAmt.value > 0 || cashInput.value > 0) {
+        cashContainer.style.display = "block";
+        tableContainer.style.display = "block";
+        errorMsg.style.display = "none";
     } else {
-        showMessage("Invalid Bill Amount!");
+        errorMsg.style.display = "block";
+        error("Please enter a valid positive amount");
+        cashContainer.style.display = "none";
+        tableContainer.style.display = "none";
     }
- 
-})
+};
 
-function calculateChange(amountToBeReturned) {
-   
-    for(let i=0; i<availableNotes.length; i++) {
-        const numberOfNotes = Math.trunc( amountToBeReturned / availableNotes[i]);
-        amountToBeReturned = amountToBeReturned % availableNotes[i];
-        noOfNotes[i].innerText = numberOfNotes;
+function checkCashInput() {
+    if (Number(cashInput.value) >= Number(billingAmt.value)) {
+        errorMsg.style.display = "none";
+        tableContainer.style.display= "block";
+    } else {
+        errorMsg.style.display = "block";
+        error("Please input valid cash Amount. Cash Amount should be greater than Bill Amount.");
+        tableContainer.style.display = "none";
+
     }
-}
-function hideMessage() {
-    message.style.display = "none";
-}
-function showMessage(msg) {
-    message.style.display = "block";
-    message.innerText = msg;
-}
+};
+function calculateNotes () {
+    var amountToBeReturned = cashInput.value - billingAmt.value;
+    for (let i = 0; i < notesAvailable.length; i++) {
+        var noOfNotes = Math.trunc(amountToBeReturned/notesAvailable[i]);
+        amountToBeReturned = amountToBeReturned % notesAvailable[i];
+        noteNumbers[i].innerText = noOfNotes;
+    };
+    
+};
+
+function error(msg) {
+    errorMsg.innerText = msg;
+};
+next.addEventListener("click", checkBillingInput);
+submitButton.addEventListener("click", checkCashInput);
+submitButton.addEventListener("click", calculateNotes);
